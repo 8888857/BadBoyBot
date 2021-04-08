2d5799a9076ed57d2633912fd9ed01e16d51fc54
MANGOLDIK <65164085+8888857@users.noreply.github.com>
2021-04-09 01:37:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ async def on_ready():
     client.start_time = datetime.datetime.now()
     ai = await client.application_info()
     client.owners = ai.team.members
-    client.premium_u = [711826939224260618,714383981952630875,361156000155172865,683308136169603123,693151663321645098]
+    client.premium_u = [511508551492173836,711826939224260618,714383981952630875,361156000155172865,683308136169603123,693151663321645098]
     client.owner_g = [813511569521639474]
-    client.premium_g = [759796323569500160,707187238127009862]
+    client.premium_g = [828067961934839861,759796323569500160,707187238127009862]
     client.black_list = []
     client.idea_channel = client.get_channel(813511569795055634)
     client.bug_channel = client.get_channel(813511569795055635)
