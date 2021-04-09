dd2fad73de4af2a38732c1b1e79c72f0fb6989e4
MANGOLDIK <65164085+8888857@users.noreply.github.com>
2021-04-09 12:02:57 +0300

    Update info.py

--- Cogs/info.py
+++ Cogs/info.py
@@ -216,7 +216,7 @@ class info(commands.Cog, name="Информация"):
         users=len(self.client.users)
         commands=len(self.client.commands)
         time = datetime.datetime.now()
-        msg = await ctx.reply(embed=discord.Embed(colour=config.COLORS['SUCCESS']))
+        msg = await ctx.reply(embed=discord.Embed(title="bot", description="загрузка...",colour=config.COLORS['SUCCESS']))
         emb= discord.Embed(title="Информация о боте",description= f"Я - Discord бот {self.client.user.mention}.\n Сейчас я умею делать немного вещей, но мой создатель постоянно меня улучшает и добавляет в меня новые функции.",colour=config.COLORS['BASE'])
         emb.add_field(name="Создатель:",value=self.client.owners[1].mention)
         emb.add_field(name="Разработчик(и):",value=self.client.owners[0].mention)
@@ -239,7 +239,7 @@ class info(commands.Cog, name="Информация"):
         )
     async def _ping(self, ctx):
         time = datetime.datetime.now()
-        msg = await ctx.reply(embed=discord.Embed(colour=config.COLORS['SUCCESS']))
+        msg = await ctx.reply(title="ping", description="загрузка...",embed=discord.Embed(colour=config.COLORS['SUCCESS']))
         await msg.edit(embed=discord.Embed(colour=config.COLORS['BASE']).add_field(name="ping WebSocket:",value=f"{round(self.client.latency, 3)} сек").add_field(name="ping Discord API:",value=f"{str(round((datetime.datetime.now() - time).total_seconds(), 3))} сек"))
     
     @commands.command(
