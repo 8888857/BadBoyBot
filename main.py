d38778adc22c514baf2b194c92d353d9b25e39ae
MANGOLDIK <65164085+8888857@users.noreply.github.com>
2021-04-24 12:01:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord import utils
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+from discord_slash import SlashCommand
 from utils import DATABASE as DB
 from discord.utils import get
 import os
@@ -13,7 +14,9 @@ import config
 from config import timeformMSK
 from config import deltaMSK
 
+
 client = commands.AutoShardedBot(shard_count=1, command_prefix = DB.Get().prefix,  intents = discord.Intents.all(), case_insensitive = True)
+slash = SlashCommand(client, sync_commands=True)
 
 client.remove_command('help')
 
@@ -30,6 +33,45 @@ async def on_ready():
     client.bug_channel = client.get_channel(813511569795055635)
     client.review_channel = client.get_channel(827809033188278292)
     client.on_off_channel = client.get_channel(813511570529320961)
+    client.error_channel = client.get_channel(832596225118830673)
+    client.guilds_id = [guild.id for guild in client.guilds]
+    client.COLORS = {
+        'BASE': 0xFF8000,
+        'SUCCESS': 0x80FF00,
+        'ERROR': 0xFF0700,
+        
+        
+        'red': 0xff1a1a,
+        'dark_red': 0x992d22,
+        
+        'blue': 0x00ccff,
+        'dark_blue': 0x206694,
+        
+        'orange': 0xff9100,
+        'dark_orange': 0xa84300,
+        
+        'purple': 0x9b00ff,
+        'dark_purple': 0x71368a,
+        
+        'green': 0x22ff00,
+        'dark_green': 0x1f8b4c,
+        
+        'teel': 0x1abc9c,
+        'dark_teel': 0x11806a,
+        
+        'grey': 0x979c9f,
+        'dark_grey': 0x607d8b, 
+        
+        'magenta': 0xe91e63, 
+        'dark_magenta': 0xad1457,
+        
+        'gold': 0xf1c40f,
+        'dark_gold': 0xc27c0e
+    }
+    client.EMOJIS = {
+        'SUCCESS': client.get_emoji(835420636490235905),
+        'ERROR': client.get_emoji(835420703029329931)
+    }
     for cog in os.listdir('./Cogs'):
         if cog not in config.COGS_IGNORE:
             if cog.endswith('.py'):
@@ -40,7 +82,7 @@ async def on_ready():
     print(f'Пинг: {round(client.latency, 3)} секунд')
     print('Я готов к работе')
     print("---------------------")
-    discord.Webhook.partial(823524857118457908,"WLGIkThKQ89Xqcczs7soJt3C9iQu8stCfeL6k88npB2S9N_eKheejLHL4eJ_ZVHt5U57", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\n{client.user}\n{len(client.guilds)} серверов\nПинг: {round(client.latency, 3)} секунд\n**запущен**\n**------------------------------------**",colour=config.COLORS['SUCCESS']))
+    discord.Webhook.partial(823524857118457908,"WLGIkThKQ89Xqcczs7soJt3C9iQu8stCfeL6k88npB2S9N_eKheejLHL4eJ_ZVHt5U57", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\n{client.user.mention}\n{len(client.guilds)} серверов\nПинг: {round(client.latency, 3)} секунд\n**запущен**\n**------------------------------------**",colour=config.COLORS['SUCCESS']))
     while True:
           await client.change_presence(status=discord.Status.online, activity=discord.Game(f"{config.prefix}help"))
           await sleep(30)
@@ -49,15 +91,26 @@ async def on_ready():
 
 @client.event
 async def on_disconnect():
-    discord.Webhook.partial(823524874513416252,"eLGwHh4OLdjGeDTTvxd9WvPZ2DqEQLfZRXG2TFhBy_P_LWrEzUQ6JYix1IfHZ6lT-3fR", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\n{client.user}\n{len(client.guilds)} серверов\n**выключен**\n**------------------------------------**",colour=config.COLORS['ERROR']))
+    print("---------------------")
+    print(f'{client.user}')
+    print(f'{len(client.guilds)} сервера')
+    print('выключен.')
+    print("---------------------")
+    discord.Webhook.partial(823524874513416252,"eLGwHh4OLdjGeDTTvxd9WvPZ2DqEQLfZRXG2TFhBy_P_LWrEzUQ6JYix1IfHZ6lT-3fR", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\n{client.user.mention}\n{len(client.guilds)} серверов\n**выключен**\n**------------------------------------**",colour=config.COLORS['ERROR']))
 
 @client.event
 async def on_shard_connect(shard_id):
-    discord.Webhook.partial(823524881139892264, "-qckgNwCgOlqCrZjCQJTNTatH_XTqWT_Ulw0zH1rep-ymDzM0nAG8jemTjGp8NLlXKP5", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"Шард с ID {shard_id} успешно запущен",colour=config.COLORS['SUCCESS']))
+    print("---------------------")
+    print(f"Шард\nбота {client.user}\n№ {int(shard_id) + 1}\nID {shard_id} \nуспешно запущен")
+    print("---------------------")
+    discord.Webhook.partial(823524881139892264, "-qckgNwCgOlqCrZjCQJTNTatH_XTqWT_Ulw0zH1rep-ymDzM0nAG8jemTjGp8NLlXKP5", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\nШард\nбота {client.user.mention}\n№ {int(shard_id) + 1}\nID {shard_id} \nуспешно запущен\n**------------------------------------**",colour=config.COLORS['SUCCESS']))
 
 @client.event
 async def on_shard_disconnect(shard_id):
-    discord.Webhook.partial(823524888945098812, "Kg4oKTQSzxrq7foJ0-E1NMrTJH69esLbOl1EWSnOzI-7LDTIZhQMSxzhsKwwBjq_kMsS", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"Шард с ID {shard_id} отключён",colour=config.COLORS['ERROR']))
+    print("---------------------")
+    print(f"Шард\nбота {client.user}\n№ {int(shard_id) + 1}\nID {shard_id} \nотключён")
+    print("---------------------")
+    discord.Webhook.partial(823524888945098812, "Kg4oKTQSzxrq7foJ0-E1NMrTJH69esLbOl1EWSnOzI-7LDTIZhQMSxzhsKwwBjq_kMsS", adapter = discord.RequestsWebhookAdapter()).send(embed=discord.Embed(description=f"**------------------------------------**\nШард\nбота {client.user.mention}\n№ {int(shard_id) + 1}\nID {shard_id} \nотключён\n**------------------------------------**",colour=config.COLORS['ERROR']))
 
 if __name__ == '__main__':
     client.run(config.TOKEN)
