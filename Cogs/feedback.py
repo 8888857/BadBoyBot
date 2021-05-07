4af13b93c5fec364b3356bb016063e2d2f8f7db9
MANGOLDIK <65164085+8888857@users.noreply.github.com>
2021-05-07 20:59:58 +0300

    Update feedback.py

--- Cogs/feedback.py
+++ Cogs/feedback.py
@@ -18,20 +18,20 @@ class FeedBack(commands.Cog):
         aliases=["idea"],
         description="• идея сделать кулдаун на команду идея меньше"
         )
-    @commands.cooldown(1, 120, commands.BucketType.user)
+    @commands.cooldown(3, 600, commands.BucketType.user)
     async def _idea(self, ctx, * , idea):
         prefix = config.prefix
         if ctx.author.id in self.client.black_list:
-            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=config.COLORS['ERROR']))
+            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=self.client.COLORS['ERROR']))
         else:
-            emb = discord.Embed(title= f"команду {prefix}идея", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{idea}\n```", colour=config.COLORS['BASE'])
+            emb = discord.Embed(title= f"команду {prefix}идея", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{idea}\n```", colour=self.client.COLORS['BASE'])
             emb.set_footer(text = f"{ctx.guild.name}\nid-{ctx.guild.id}" if ctx.guild is not None else "~~ЛС", icon_url = ctx.guild.icon_url_as(static_format = "jpg") if ctx.guild is not None else ctx.author.avatar_url_as(static_format = "jpg"))
             msg = await self.client.CHANNELS["idea"].send(embed=emb)
-            await msg.add_reaction('✅')
-            await msg.add_reaction('⚪')
-            await msg.add_reaction('❌')
-            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=config.COLORS['BASE']))
+            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=self.client.COLORS['BASE']))
             await ctx.message.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['offline'])
+            await msg.add_reaction(self.client.EMOJIS['ERROR'])
     
     @commands.command(
         name="баг",
@@ -40,20 +40,20 @@ class FeedBack(commands.Cog):
         aliases=["bug","багулина"],
         description="• баг можно пинговать через say"
         )
-    @commands.cooldown(1, 120, commands.BucketType.user)
+    @commands.cooldown(3, 600, commands.BucketType.user)
     async def _bug(self, ctx, * , bug):
         prefix = config.prefix
         if ctx.author.id in self.client.black_list:
-            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=config.COLORS['ERROR']))
+            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=self.client.COLORS['ERROR']))
         else:
-            emb = discord.Embed(title= f"команду {prefix}баг", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{bug}\n```", colour=config.COLORS['BASE'])
+            emb = discord.Embed(title= f"команду {prefix}баг", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{bug}\n```", colour=self.client.COLORS['BASE'])
             emb.set_footer(text = f"{ctx.guild.name}\nid-{ctx.guild.id}" if ctx.guild is not None else "~~ЛС", icon_url = ctx.guild.icon_url_as(static_format = "jpg") if ctx.guild is not None else ctx.author.avatar_url_as(static_format = "jpg"))
             msg = await self.client.CHANNELS['bug'].send(embed=emb)
-            await msg.add_reaction('✅')
-            await msg.add_reaction('⚪')
-            await msg.add_reaction('❌')
-            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=config.COLORS['BASE']))
+            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=self.client.COLORS['BASE']))
             await ctx.message.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['offline'])
+            await msg.add_reaction(self.client.EMOJIS['ERROR'])
             
     @commands.command(
         name="отзыв",
@@ -62,20 +62,20 @@ class FeedBack(commands.Cog):
         aliases=["review"],
         description="• отзыв хороший бот 100500/10"
         )
-    @commands.cooldown(1, 120, commands.BucketType.user)
+    @commands.cooldown(3, 600, commands.BucketType.user)
     async def _rewiew(self, ctx, * , review):
         prefix = config.prefix
         if ctx.author.id in self.client.black_list:
-            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=config.COLORS['ERROR']))
+            return await ctx.reply(embed=discord.Embed(description=f"**{ctx.author.mention}**,\n эта команда для вас заблокирована.", colour=self.client.COLORS['ERROR']))
         else:
-            emb = discord.Embed(title= f"команду {prefix}отзыв", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{review}\n```", colour=config.COLORS['BASE'])
+            emb = discord.Embed(title= f"команду {prefix}отзыв", description= f"**использовал:\n{ctx.author}**\n`(id-{ctx.author.id})`\nтекст:\n```\n{review}\n```", colour=self.client.COLORS['BASE'])
             emb.set_footer(text = f"{ctx.guild.name}\nid-{ctx.guild.id}" if ctx.guild is not None else "~~ЛС", icon_url = ctx.guild.icon_url_as(static_format = "jpg") if ctx.guild is not None else ctx.author.avatar_url_as(static_format = "jpg"))
             msg = await self.client.CHANNELS['review'].send(embed=emb)
-            await msg.add_reaction('✅')
-            await msg.add_reaction('⚪')
-            await msg.add_reaction('❌')
-            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=config.COLORS['BASE']))
+            await ctx.reply(embed = discord.Embed(description=f"**{ctx.message.author.mention}**,\n спасибо за ваш вклад в развитие бота", colour=self.client.COLORS['BASE']))
             await ctx.message.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['SUCCESS'])
+            await msg.add_reaction(self.client.EMOJIS['offline'])
+            await msg.add_reaction(self.client.EMOJIS['ERROR'])
     
 def setup(client):
     client.add_cog(FeedBack(client))
